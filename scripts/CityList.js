bb3725638039c55a2489b3c9251372aadc4d373c
jmason87 <joseph.mason87@gmail.com>
2021-10-18 11:09:05 -0500

    intial commit

---
+++ scripts/CityList.js
@@ -0,0 +1,19 @@
+import { getWalkers } from "./database.js"
+import { getCities } from "./database.js"
+
+const cities = getCities()
+const walkers = getWalkers()
+
+
+export const CityList = () => {
+    let citiesHTML = "<ul>"
+
+    for (const city of cities) {
+        citiesHTML += `<li>${city.name}</li>`
+    }
+
+    citiesHTML += "</ul>"
+
+    return citiesHTML
+}
+
---
+++ scripts/RegisteredPets.js
@@ -0,0 +1,46 @@
+import { getPets } from "./database.js"
+import { getWalkers} from "./database.js"
+
+
+   document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+
+                    let assignedWalker = walkers.find(assignedWalker, assignedWalker.id, walkers)
+
+                    }
+                    window.alert(`${pet.name} is being walked by ${assignedWalker.name}`)
+                }
+            }
+        }
+   )
+
+   
+
+
+//    let result = arr.find(function(item, index, array) {
+//     // if true is returned, item is returned and iteration is stopped
+//     // for falsy scenario returns undefined
+//   });
+
+const walkers = getWalkers()
+const pets = getPets()
+
+export const RegisteredPets = () => {
+    let petHTML = "<ul>"
+
+    for (const pet of pets) {
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
+    }
+
+    petHTML += "</ul>"
+
+    return petHTML
+}
+
